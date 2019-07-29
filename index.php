f872a3104c53cdfeb7c25e4a561180f81e662115
Benoit DAVID <benoit.davi@free.fr>
2019-07-29 15:26:04 +0000

    intégration JSON-LD

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
 /*PhpDoc:
 name: index.php
-title: index.php - exposition HTML+RDFa des catalogues de données
+title: index.php - exposition HTML+JSON-LD des catalogues de données
 functions:
 doc: |
   Chaque ressource est identifiée par un URI du type http://id.georef.eu/{catid}/{resid}
   Elle est présentée par une page HTML+RDFa index.php?cat={catid}&res={resid}
 journal:
+  29/7/2019:
+    - intégration du JSON-LD
   26/7/2019:
     - première version
+    - la visualisation fonctionne en localhost et sur georef.eu
+    - il reste à la paufiner et à intégrer les JSON-LD
 */
 //print_r($_SERVER);
 
@@ -22,53 +26,50 @@ if (!isset($_GET['cat'])) {
   echo "</meta>\n";
   echo "Accueil donnant accès aux différents catalogues<br>\n";
   echo "<ul>\n";
-  echo "<li><a href='?cat=mtescat'>Inventaire des données du MTES & MCTRCT</a>",
+  echo "<li><a href='?cat=cdmet'>Catalogue des données des ministères de l'environnement et des territoires</a>",
     " repris de <a href='https://mtes-mct.github.io/dataroom/'>dataroom</a></li>\n";
   echo "</ul>";
   die();
 }
 
-function showObj(string $obj): string {http:///mtescat/dataset/ENPA
-  if (preg_match('!^http://(localhost/yamldoc/id.php|id.georef.eu)/([^/]+)/(.*)$!', $obj, $matches))
+// retourne la chaine à afficher pour un obj pour affichage d'un lien interne ou externe
+function showObj(string $obj): string {
+  if (preg_match('!^http://(localhost/yamldoc/id.php|id.georef.eu)/([^/]+)/(.*)$!', $obj, $matches)) // lien interne
     return "<a href='?cat=$matches[2]&amp;res=".urlencode($matches[3])."'>$obj</a>";
-  elseif (preg_match('!^(https?://[^ ]+)!', $obj, $matches))
+  elseif (preg_match('!^(https?://[^ ]+)!', $obj, $matches)) // lien externe
     return "<a href='$matches[1]' target='_blank'>$obj</a>";
   else
     return $obj;
 }
 
-// Description du catalogue
-if (!isset($_GET['res'])) {
-  echo "<!DOCTYPE html>\n";
-  echo "<html lang=\"fr\">\n";
-  echo "<head>\n";
-  echo "<meta charset=\"utf-8\">\n";
-  echo "<title>dcat $_GET[cat]</title>\n";
-  echo "</meta>\n";
-  echo "Accueil du catalogue $_GET[cat]<br>\n";
-  $rdfuri = ($_SERVER['HTTP_HOST']=='localhost') ? "http://localhost/yamldoc/id.php" : "http://id.georef.eu";
-  $rdfuri .= "/$_GET[cat]/catalog";
-}
+// Description du catalogue ou d'une ressource du catalogue
+$cat = $_GET['cat'];
+$resource = $_GET['res'] ?? null;
+$rdfuri = ($_SERVER['HTTP_HOST']=='localhost') ? "http://localhost/yamldoc/id.php" : "http://id.georef.eu";
+$rdfuri .= "/$cat/".($resource ?? 'catalog');
 
-// Description d'une ressource du catalogue
-else {
-  echo "<!DOCTYPE html>\n";
-  echo "<html lang=\"fr\">\n";
-  echo "<head>\n";
-  echo "<meta charset=\"utf-8\">\n";
-  echo "<title>dcat $_GET[cat]</title>\n";
-  echo "</meta>\n";
-  echo "Affichage de la ressource $_GET[res] du catalogue $_GET[cat]<br>\n";
-  $rdfuri = ($_SERVER['HTTP_HOST']=='localhost') ? "http://localhost/yamldoc/id.php" : "http://id.georef.eu";
-  $rdfuri .= "/$_GET[cat]/$_GET[res]";
-}
-
-echo "rdfuri=$rdfuri<br>\n";
+//echo "rdfuri=$rdfuri<br>\n";
 $data = json_decode(file_get_contents($rdfuri), true);
-echo "<pre>data="; print_r($data); echo "</pre>\n";
+
+echo "<!DOCTYPE html>\n";
+echo "<html lang=\"fr\">\n";
+echo "<head>\n";
+echo "<meta charset=\"utf-8\">\n";
+echo "<title>dcat $_GET[cat]</title>\n";
+echo "<script type=\"application/ld+json\">\n";
+echo json_encode($data, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+echo "\n</script>\n";
+echo "</meta></head>\n";
+if (!$resource)
+  echo "Accueil du catalogue $cat<br>\n";
+else
+  echo "Affichage de la ressource $resource du catalogue $cat<br>\n";
+//echo "<pre>data="; print_r($data); echo "</pre>\n";
 echo "<table border=1>";
 foreach ($data as $prop => $objs) {
-  if (!is_array($objs))
+  if ($prop == '@context')
+    echo "<tr><td>$prop</td><td>$objs</td></tr>\n";
+  elseif (!is_array($objs))
     echo "<tr><td>$prop</td><td>",showObj($objs),"</td></tr>\n";
   else {
     foreach ($objs as $obj)
